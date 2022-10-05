812c0fd037c2f01de2f8491e8d4ed287603b2212
jerryWTMH <jerry1202.wtmh@gmail.com>
2022-10-05 12:00:13 -0400

    ddd

--- problem3/bandwidth.c
+++ problem3/bandwidth.c
@@ -5,6 +5,7 @@
 
 const int stride = 16;
 int num_elements;
+int num_traversals;
 uint64_t *array;
 uint64_t *array2;
 uint64_t *array3;
@@ -26,48 +27,56 @@ double calc_time(struct timespec start, struct timespec end) {
 void init_array(){
   for(int i=0; i < num_elements; i++){
     array[i] = i + 1;
-    array2[i] = i + 100000000;
-    array3[i] = i + 10000000;
-    array4[i] = i + 1000000;
-    array5[i] = i + 100000;
-    array6[i] = i + 10000;
+    array2[i] = i + 1000;
+    array3[i] = i + 10000;
   }
 }
 
 void write_traffic(){
+  printf("===========================================================================\n");
   int i;
   clock_gettime(CLOCK_MONOTONIC, &start_time);
-  for (i = 0; i < (num_elements); i++){
-    array[i] = 1000;
+  for(int j = 0; j < num_traversals; j++){
+    for (int i = 0; i < (num_elements); i++){
+      array[i] = 1000;
+    }
   }
   clock_gettime(CLOCK_MONOTONIC, &end_time);
   double result = calc_time(start_time, end_time);
-  printf("Write Traffice Only Time Cost: %f\n", result);
-  printf("Write Traffice Only Bandwidth: %f\n", num_elements/ result);
-  
+  printf("Write Traffice Only Time Cost: %f (ns)\n", result);
+  double time = 1.0 * num_elements * num_traversals * sizeof(uint64_t) / result;
+  printf("Write Traffice Only Bandwidth: %f (GB/s)\n", time);
+  printf("===========================================================================\n");
 }
 
 void read_write_one_traffic(){
   clock_gettime(CLOCK_MONOTONIC, &start_time);
-   for (int i = 0; i < (num_elements); i++){
-    array2[i] = array3[i];
+  for(int j = 0; j < (num_traversals); j++){
+    for (int i = 0; i < (num_elements); i++){
+      array[i] = array2[i];
+    }
   }
   clock_gettime(CLOCK_MONOTONIC, &end_time);
   double result = calc_time(start_time, end_time);
-  printf("One Read One Write Time Cost: %f\n", result);
-  printf("One Read One Write Bandwidth: %f\n", (2 * num_elements / result));
+  printf("One Read One Write Time Cost: %f (ns)\n", result);
+  double time = 2.0 * num_elements * num_traversals * sizeof(uint64_t)/ result;
+  printf("One Read One Write Bandwidth: %f (GB/s)\n", time);
+  printf("===========================================================================\n");
 }
 
 void read_write_two_traffic(){
   clock_gettime(CLOCK_MONOTONIC, &start_time);
-   for (int i = 0; i < (num_elements); i++){
-    array4[i] = array5[i] + array6[i];
+  for(int j = 0; j < num_traversals; j++){
+    for (int i = 0; i < (num_elements); i++){
+      array[i] = array2[i] + array3[i];
+    }
   }
   clock_gettime(CLOCK_MONOTONIC, &end_time);
   double result = calc_time(start_time, end_time);
-  printf("Two Read One Write Time Cost: %f\n", result);
-  printf("Two Read One Write Bandwidth: %f\n", (3 * num_elements / result));
-
+  printf("Two Read One Write Time Cost: %f (ns)\n", result);
+  double time = 3.0 * num_elements * num_traversals * sizeof(uint64_t) / result;
+  printf("Two Read One Write Bandwidth: %f (GB/s)\n", time);
+  printf("===========================================================================\n");
 }
 
 
@@ -78,13 +87,10 @@ int main(int argc, char *argv[]) {
   struct timespec start_time, end_time;
   
   num_elements = atoi(argv[1]); // 400000000
-  //num_traversals = atoi(argv[2]);
+  num_traversals = atoi(argv[2]);
   array = (uint64_t*)malloc(num_elements * sizeof(uint64_t) * stride);
   array2 = (uint64_t*)malloc(num_elements * sizeof(uint64_t) * stride);
   array3 = (uint64_t*)malloc(num_elements * sizeof(uint64_t) * stride);
-  array4 = (uint64_t*)malloc(num_elements * sizeof(uint64_t) * stride);
-  array5 = (uint64_t*)malloc(num_elements * sizeof(uint64_t) * stride);
-  array6 = (uint64_t*)malloc(num_elements * sizeof(uint64_t) * stride);
   init_array();
   write_traffic();
   read_write_one_traffic();
